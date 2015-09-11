18f450c74e404fa144158c90c7d3c1c2de3c5851
Mark Evans <mark@new-bamboo.co.uk>
2015-09-11 18:02:17 +0100

    use PureRenderMixin

--- src/component_methods.js
+++ src/component_methods.js
@@ -1,21 +1,4 @@
-let areEqual = (var1, var2) => {
-  if ( var1.equals ) {
-    return var1.equals(var2)
-  } else {
-    return var1 == var2
-  }
-}
-
-// assumes objects are the same length
-let elementsAreEqual = (obj1, obj2) => {
-  let key
-  for ( key in obj1 ) {
-    if ( !areEqual(obj1[key], obj2[key]) ) {
-      return false
-    }
-  }
-  return true
-}
+let PureRenderMixin = require('react/addons').addons.PureRenderMixin
 
 let componentMethods = (app) => {
   return {
@@ -107,10 +90,7 @@ let componentMethods = (app) => {
       this.initForTreehouse()
     },
 
-    shouldComponentUpdate (nextProps, nextState) {
-      return !elementsAreEqual(this.state, nextState) ||
-        !elementsAreEqual(this.props, nextProps)
-    },
+    shouldComponentUpdate: PureRenderMixin.shouldComponentUpdate,
 
     componentDidUpdate () {
       this.markCleanWithDirtyTracker()
