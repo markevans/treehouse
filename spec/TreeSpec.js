9e37c7339952c0abe805d2f283918b1d8e829ecc
Mark Evans <markevans3000@gmail.com>
2017-10-12 11:45:50 +0100

    Tree applyChanges

--- spec/TreeSpec.js
+++ spec/TreeSpec.js
@@ -29,6 +29,52 @@ describe("Tree", () => {
     })
   })
 
+  describe("applying changes", () => {
+    let change1, change2
+
+    beforeEach(() => {
+      tree.init({
+        users: [
+          {name: 'Rogdolph', age: 137},
+          {name: 'Gungsteed', age: 5},
+        ],
+        modalIsOpen: false
+      })
+      change1 = { path: ['users', 1, 'age'], value: 438 }
+      change2 = { path: ['modalIsOpen'], value: true }
+    })
+
+    it("applies each change", () => {
+      tree.push(change1)
+      tree.push(change2)
+      tree.applyChanges()
+      expect(tree.pull()).toEqual({
+        users: [
+          {name: 'Rogdolph', age: 137},
+          {name: 'Gungsteed', age: 438},
+        ],
+        modalIsOpen: true
+      })
+    })
+
+    it("creates new objects", () => {
+      const usersBefore = tree.pull().users,
+        user1Before = usersBefore[1]
+      tree.push(change1)
+      tree.applyChanges()
+      const users = tree.pull().users,
+        user1 = users[1]
+      expect(usersBefore === users).toBeFalsy()
+      expect(user1Before === user1).toBeFalsy()
+    })
+
+    it("empties changes afterwards", () => {
+      tree.push(change1)
+      tree.applyChanges()
+      expect(tree.changes()).toEqual([])
+    })
+  })
+
   describe("at", () => {
     it("returns a new cursor with the new path", () => {
       let cursor = tree.at(['new', 1])
