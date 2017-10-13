83c746f5cd3beb90d52ea5c321e8eb37ad830f37
Mark Evans <markevans3000@gmail.com>
2017-10-13 12:45:50 +0100

    allow registering just a function for filters, and raise if setting with no unfilter

--- spec/FilteredPipeSpec.js
+++ spec/FilteredPipeSpec.js
@@ -9,25 +9,53 @@ describe("FilteredPipe", () => {
     source = { push: null, pull: null, channels: null }
     spec = { filter: null, unfilter: null }
     args = { some: 'arg' }
-    filteredPipe = new FilteredPipe(app, source, name, spec, args)
   })
 
-  it("returns the filtered data", () => {
-    spyOn(source, 'pull').and.returnValue('value')
-    spec.filter = (val, args) => val.toUpperCase() + args.some
-    expect(filteredPipe.pull()).toEqual('VALUEarg')
+  describe("with filter and unfilter", () => {
+
+    beforeEach(() => {
+      filteredPipe = new FilteredPipe(app, source, name, spec, args)
+    })
+
+    it("returns the filtered data", () => {
+      spyOn(source, 'pull').and.returnValue('value')
+      spec.filter = (val, args) => val.toUpperCase() + args.some
+      expect(filteredPipe.pull()).toEqual('VALUEarg')
+    })
+
+    it("back-propagates changes", () => {
+      const spy = spyOn(source, 'push')
+      spec.unfilter = (val, args) => val.replace(args.some, '').toLowerCase()
+      filteredPipe.push('VALUEarg')
+      expect(source.push).toHaveBeenCalledWith('value')
+    })
+
+    it("delegates channels to its source", () => {
+      spyOn(source, 'channels').and.returnValue(new Set(['egg', 'heads']))
+      expect(filteredPipe.channels()).toEqual(new Set(['egg', 'heads']))
+    })
+
   })
 
-  it("back-propagates changes", () => {
-    const spy = spyOn(source, 'push')
-    spec.unfilter = (val, args) => val.replace(args.some, '').toLowerCase()
-    filteredPipe.push('VALUEarg')
-    expect(source.push).toHaveBeenCalledWith('value')
-  })
+  describe("when only a function is given", () => {
 
-  it("delegates channels to its source", () => {
-    spyOn(source, 'channels').and.returnValue(new Set(['egg', 'heads']))
-    expect(filteredPipe.channels()).toEqual(new Set(['egg', 'heads']))
+    beforeEach(() => {
+      spec = jasmine.createSpy('filter')
+      filteredPipe = new FilteredPipe(app, source, name, spec, args)
+    })
+
+    it("allows just giving a filter function", () => {
+      spyOn(source, 'pull').and.returnValue('value')
+      spec.and.returnValue('return value')
+      expect(filteredPipe.pull()).toEqual('return value')
+      expect(spec).toHaveBeenCalledWith('value', { some: 'arg' })
+    })
+
+    it("throws if tried to push", () => {
+      expect(() => {
+        filteredPipe.push('thing')
+      }).toThrowError("You need to implement 'unfilter' on the 'someName' filter to be able to set through it")
+    })
   })
 
 })
