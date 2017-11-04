5f31156055c83780d868b9a3885fb3315af100a7
Mark Evans <markevans3000@gmail.com>
2017-11-04 21:36:25 +0000

    integration spec for using treeViews

---
+++ spec/integration/usingTreeViewsSpec.js
@@ -0,0 +1,57 @@
+const App = require('../../lib/App')
+
+describe("Using treeViews", () => {
+
+  let app
+
+  beforeEach(() => {
+    app = new App()
+  })
+
+  describe("using a treeview to connect to a part of the tree", () => {
+
+    let treeView
+
+    beforeEach(() => {
+      app.init({
+        settings: {
+          page: 7
+        },
+        isLoading: false
+      })
+      treeView = app.pick(t => ({
+        page: t.at('settings', 'page'),
+        loading: t.at('isLoading')
+      }))
+    })
+
+    it("gets data", () => {
+      expect(treeView.get()).toEqual({
+        page: 7,
+        loading: false
+      })
+    })
+
+    it("updates when data is changed", () => {
+      let results
+      treeView.watch(({page, loading}) => {
+        results = [page, loading]
+      })
+      app.tree.push({path: ['settings', 'page'], value: 6})
+      app.commitChanges()
+      expect(results).toEqual([6, false])
+    })
+
+    it("doesn't update when specific data hasn't changed", () => {
+      let results
+      treeView.watch(({page, loading}) => {
+        results = [page, loading]
+      })
+      app.tree.push({path: ['other', 'path'], value: 6})
+      app.commitChanges()
+      expect(results).toBeUndefined()
+    })
+
+  })
+
+})
