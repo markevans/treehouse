5d7ea581c747b6815627e5097e917fa1e623328d
Mark Evans <markevans3000@gmail.com>
2017-11-04 22:35:19 +0000

    Cursor uses tree off of app that's passed in

--- spec/CursorSpec.js
+++ spec/CursorSpec.js
@@ -1,29 +1,30 @@
 const Cursor = require('../lib/Cursor')
 
 describe("Cursor", () => {
-  let app, tree
+  let app
 
   beforeEach(() => {
-    app = {}
-    tree = {pull: null, push: null}
+    app = {
+      tree: {pull: null, push: null}
+    }
   })
 
   describe("pulling data", () => {
     it("gets nested data", () => {
-      spyOn(tree, 'pull').and.returnValue({animal: {type: 'Dog'}})
-      const cursor = new Cursor(app, tree, ['animal', 'type'])
+      spyOn(app.tree, 'pull').and.returnValue({animal: {type: 'Dog'}})
+      const cursor = new Cursor(app, ['animal', 'type'])
       expect(cursor.pull()).toEqual('Dog')
     })
 
     it("works fine with arrays", () => {
-      spyOn(tree, 'pull').and.returnValue({users: ['Sam', 'Henry', 'Daisy']})
-      const cursor = new Cursor(app, tree, ['users', 1])
+      spyOn(app.tree, 'pull').and.returnValue({users: ['Sam', 'Henry', 'Daisy']})
+      const cursor = new Cursor(app, ['users', 1])
       expect(cursor.pull()).toEqual('Henry')
     })
 
     it("returns undefined if the path doesn't match anything", () => {
-      spyOn(tree, 'pull').and.returnValue({bligh: 'blagh'})
-      const cursor = new Cursor(app, tree, ['doobie', 'mcgovern'])
+      spyOn(app.tree, 'pull').and.returnValue({bligh: 'blagh'})
+      const cursor = new Cursor(app, ['doobie', 'mcgovern'])
       expect(cursor.pull()).toBeUndefined()
     })
   })
@@ -31,10 +32,10 @@ describe("Cursor", () => {
   describe("pushing data", () => {
 
     it("sets attributes", () => {
-      const cursor = new Cursor(app, tree, ['animal', 'type'])
-      spyOn(tree, 'push')
+      const cursor = new Cursor(app, ['animal', 'type'])
+      spyOn(app.tree, 'push')
       cursor.push('Lion')
-      expect(tree.push).toHaveBeenCalledWith({path: ['animal', 'type'], value: 'Lion'})
+      expect(app.tree.push).toHaveBeenCalledWith({path: ['animal', 'type'], value: 'Lion'})
     })
 
   })
@@ -43,7 +44,7 @@ describe("Cursor", () => {
     let cursor
 
     beforeEach(() => {
-      cursor = new Cursor(app, tree, ['users'])
+      cursor = new Cursor(app, ['users'])
     })
 
     it("returns a new cursor with the new path", () => {
@@ -60,7 +61,7 @@ describe("Cursor", () => {
 
   describe("channels", () => {
     it("returns a set with the main bough as the single element", () => {
-      const cursor = new Cursor(app, tree, ['users', 'best', 5])
+      const cursor = new Cursor(app, ['users', 'best', 5])
       expect(cursor.channels()).toEqual(new Set(['users']))
     })
   })
