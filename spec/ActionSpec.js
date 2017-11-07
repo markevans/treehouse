5f83a3f013aeae77b70afd55b754f17a1417fff3
Mark Evans <markevans3000@gmail.com>
2017-11-07 18:13:43 +0000

    yield a getter to actions

--- spec/ActionSpec.js
+++ spec/ActionSpec.js
@@ -2,20 +2,34 @@ const Action = require('../lib/Action')
 
 describe("Action", () => {
 
-  let app, spec
+  let app, actionFunc
 
   beforeEach(() => {
     app = {
-      event: spy('app.event')
+      event: spy('app.event'),
+      tree: {
+        pull: spy('tree.pull')
+      }
     }
-    spec = spy('action')
+    actionFunc = spy('action')
   })
 
   it("performs the given action", () => {
-    action = new Action(app, 'myEvent', spec)
+    action = new Action(app, 'myEvent', actionFunc)
     action.call({somePayload: 'payload'})
 
-    expect(spec).toHaveBeenCalledWith({somePayload: 'payload'}, app.event)
+    expect(actionFunc).toHaveBeenCalledWith({somePayload: 'payload'}, app.event, jasmine.any(Function))
+  })
+
+  it("yields a getter for the tree state", () => {
+    app.tree.pull.and.returnValues(4, 8)
+    let values = []
+    action = new Action(app, 'myEvent', (payload, event, getTree) => {
+      values.push(getTree() + 1)
+      values.push(getTree() + 0.5)
+    })
+    action.call()
+    expect(values).toEqual([5, 8.5])
   })
 
 })
