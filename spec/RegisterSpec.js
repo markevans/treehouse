64b48ecbb43803c390eefdfeb0b8237d6aee6699
Mark Evans <markevans3000@gmail.com>
2017-11-12 19:59:48 +0000

    allow setting spec to null

--- spec/RegisterSpec.js
+++ spec/RegisterSpec.js
@@ -5,6 +5,7 @@ describe("Register", () => {
   let register
 
   describe("registering and finding", () => {
+
     beforeEach(() => {
       register = new Register('testThing')
       register.registerMany({
@@ -22,10 +23,16 @@ describe("Register", () => {
       }).toThrowError("Can't find testThing 'someOtherName' as it hasn't been registered")
     })
 
-    it("allows registering one", () => {
+    it("allows registering just one", () => {
       register.register('githeads', 'thing')
       expect(register.find('githeads')).toEqual('thing')
     })
+
+    it("doesn't throw if set to null", () => {
+      register.register('githeads', null)
+      expect(register.find('githeads')).toEqual(null)
+    })
+
   })
 
 })
