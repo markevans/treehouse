f5367b3b8e57512a7af1ef3289ddba0706d80e60
Mark Evans <markevans3000@gmail.com>
2018-03-13 11:57:50 +1100

    Ability to add a build function to a register

--- spec/RegisterSpec.js
+++ spec/RegisterSpec.js
@@ -2,9 +2,9 @@ const Register = require('../lib/Register')
 
 describe("Register", () => {
 
-  let register
-
   describe("registering and finding", () => {
+
+    let register
 
     beforeEach(() => {
       register = new Register('testThing')
@@ -31,6 +31,32 @@ describe("Register", () => {
     it("doesn't throw if set to null", () => {
       register.register('githeads', null)
       expect(register.find('githeads')).toEqual(null)
+    })
+
+  })
+
+  describe("adding a build function", () => {
+
+    let ChildRegister, register
+
+    beforeEach(() => {
+      class ChildRegister extends Register {
+        build (name, spec) {
+          return {builtThing: spec.some.toUpperCase()}
+        }
+      }
+      register = new ChildRegister('testChild')
+      register.register('someName', {
+        some: 'spec'
+      })
+    })
+
+    it("returns the built item", () => {
+      expect(register.find('someName')).toEqual({builtThing: 'SPEC'})
+    })
+
+    it("still returns the spec if needed", () => {
+      expect(register.findSpec('someName')).toEqual({some: 'spec'})
     })
 
   })
